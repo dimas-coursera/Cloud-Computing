6c86835dbad7bcefd1c8d609963d9db1bdf7cd0f
Dimas the Computer Engineer in the Clouds <dimasdejesus@icloud.com>
2019-02-10 21:17:35 -0800

    Add files for Python and Java languages

---
+++ HelloWorldDimas.java
@@ -0,0 +1,8 @@
+public class HelloWorldDimas{
+
+	public static void main (String[] args) {
+
+		System.out.println("This is my first program!");
+		System.out.println("Programmers are rockstars!");
+	}
+}
