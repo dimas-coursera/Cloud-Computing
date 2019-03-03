a598ec9b5b34e7f77263073615a0e49b6cd6bc29
Dimas the Computer Engineer in the Clouds <dimasdejesus@icloud.com>
2019-03-03 06:36:22 +0000

    Delete firstprog_dimas.py
    updating repo for new project 2019 for cloud computing portfolio

--- firstprog_dimas.py
+++
@@ -1,6 +0,0 @@
-print "Hello World"
-print "my first program"
-
-print "This is my first Screenshot of my first program because now I can script!"
-
-print "I used Text Wrangler, and Terminal to write and run my program."
